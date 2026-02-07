6611c799b43ccf3c808147c35c68f966fe9b9792
Dhanush Konduru <164062577+dhanushkonduru@users.noreply.github.com>
2026-02-07 10:19:26 +0530

    Time: 2 ms (93.60%) | Memory: 45.6 MB (75.34%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,23 +1,36 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
-        boolean[][] match=new boolean[s.length()+1][p.length()+1];
-        match[s.length()][p.length()]=true;
-        for(int i=p.length()-1;i>=0;i--){
-            if(p.charAt(i)!='*')
-                break;
-            else
-                match[s.length()][i]=true;
-        }
-        for(int i=s.length()-1;i>=0;i--){
-            for(int j=p.length()-1;j>=0;j--){
-                if(s.charAt(i)==p.charAt(j)||p.charAt(j)=='?')
-                        match[i][j]=match[i+1][j+1];
-                else if(p.charAt(j)=='*')
-                        match[i][j]=match[i+1][j]||match[i][j+1];
-                else
-                    match[i][j]=false;
+
+        int i = 0, j = 0;
+        int starIndex = -1;
+        int matchIndex = 0;
+
+        while (i < s.length()) {
+
+            if (j < p.length() &&
+               (p.charAt(j) == '?' || p.charAt(j) == s.charAt(i))) {
+                i++;
+                j++;
+            }
+            else if (j < p.length() && p.charAt(j) == '*') {
+                starIndex = j;
+                matchIndex = i;
+                j++;
+            }
+            else if (starIndex != -1) {
+                j = starIndex + 1;
+                matchIndex++;
+                i = matchIndex;
+            }
+            else {
+                return false;
             }
         }
-        return match[0][0];
+
+        while (j < p.length() && p.charAt(j) == '*') {
+            j++;
+        }
+
+        return j == p.length();
     }
 }
