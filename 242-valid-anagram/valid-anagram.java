7d05fb153fe3a8f7f0fcde940334e59be3331e59
Dhanush Konduru <dhanushkonduru@gmail.com>
2026-06-04 19:35:19 +0530

    Time: 26 ms (5.61%) | Memory: 44.7 MB (53.93%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -4,7 +4,7 @@ class Solution {
             return false;
         }
 
-        int[] count = new int[26];
+        /*int[] count = new int[26];
 
         for (int i = 0; i < s.length(); i++) {
             count[s.charAt(i) - 'a']++;
@@ -13,6 +13,20 @@ class Solution {
 
         for (int num : count) {
             if (num != 0) {
+                return false;
+            }
+        }*/
+
+
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) - 1);
+        }
+
+        for(Map.Entry<Character,Integer> entry : map.entrySet()){
+            if(entry.getValue() != 0){
                 return false;
             }
         }
