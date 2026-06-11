1af724b19a8c015ce742e3b316965a3a58b9318d
Dhanush Konduru <dhanushkonduru@gmail.com>
2026-06-11 20:14:00 +0530

    Time: 333 ms (92.62%) | Memory: 0B (100.00%) - LeetSync

---
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+select (a1.name)
+from Employee as a1
+join Employee as a2
+where a1.id = a2.managerId 
+GROUP BY a1.id, a2.managerId
+HAVING COUNT(*) >= 5;
